50791c41b6de6fb75e255d21bed3e34969d81afc
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-16 15:53:14 -0700

    linter editing

--- src/client.py
+++ src/client.py
@@ -3,7 +3,7 @@ import sys
 
 
 def client_send(msg):
-    """Send message and return msg received by server."""
+    """Send a message and return msg received by server."""
     infos = socket.getaddrinfo('127.0.0.1', 5000)
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
